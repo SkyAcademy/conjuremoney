da3073b21d5cfe6a64658dbec5b96a89cdf6ecad
ConjureCraft <42539420+SkyAcademy@users.noreply.github.com>
2019-08-01 16:39:58 +0200

    Create register.php

---
+++ register.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ConjureMoney - Register</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="header">
+        <h2>Register</h2>
+    </div>
+</body>
+</html>
